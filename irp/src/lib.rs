a017183e0f4075bcb818ab908a88f5e222545c21
spacemeowx2 <spacemeowx2@gmail.com>
2020-12-07 16:25:36 +0800

    Initial commit

---
+++ irp/src/lib.rs
@@ -0,0 +1,15 @@
+//! APiR(Async Proxy in Rust)
+//!
+//! Aimed to be the standard between proxy softwares written in Rust.
+
+pub trait Proxy {
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
