30ac29ca6ea2e8731c9794ef090bad62d0ac25e3
spacemeowx2 <spacemeowx2@gmail.com>
2021-03-10 01:23:54 +0800

    feat: implement ss tcp

--- ss/src/lib.rs
+++ ss/src/lib.rs
@@ -1,34 +1,91 @@
+mod wrapper;
+
 use rd_interface::{
-    async_trait, Address, Context, INet, Registry, Result, TcpListener, TcpStream, UdpSocket,
+    async_trait,
+    config::{from_value, Value},
+    Address, Arc, INet, IntoAddress, Net, Registry, Result, TcpListener, TcpStream, UdpSocket,
     NOT_IMPLEMENTED,
 };
+use serde_derive::Deserialize;
+use shadowsocks::{
+    config::{ServerConfig, ServerType},
+    context::Context,
+    ProxyClientStream,
+};
+use tokio_util::compat::*;
+use wrapper::{WrapAddress, WrapCipher, WrapSSTcp};
 
-pub struct Net;
+#[derive(Debug, Deserialize, Clone)]
+struct SSNetConfig {
+    server: String,
+    port: u16,
+    password: String,
+    #[serde(deserialize_with = "crate::wrapper::deserialize_cipher")]
+    cipher: WrapCipher,
+}
 
-impl Net {
-    fn new() -> Net {
-        Net
+pub struct SSNet {
+    net: Net,
+    context: Arc<Context>,
+    config: SSNetConfig,
+}
+
+impl SSNet {
+    fn new(net: Net, config: Value) -> rd_interface::Result<SSNet> {
+        let context = Arc::new(Context::new(ServerType::Local));
+        let config: SSNetConfig = from_value(config)?;
+        Ok(SSNet {
+            net,
+            context,
+            config,
+        })
     }
 }
 
 #[async_trait]
-impl INet for Net {
-    async fn tcp_connect(&self, _ctx: &mut Context, _addr: Address) -> Result<TcpStream> {
+impl INet for SSNet {
+    async fn tcp_connect(
+        &self,
+        ctx: &mut rd_interface::Context,
+        addr: Address,
+    ) -> Result<TcpStream> {
+        let cfg = self.config.clone();
+        let stream = self
+            .net
+            .tcp_connect(ctx, (cfg.server.as_ref(), cfg.port).into_address()?)
+            .await?
+            .compat();
+        let svr_cfg = ServerConfig::new((cfg.server, cfg.port), cfg.password, cfg.cipher.into());
+        let client = ProxyClientStream::from_stream(
+            self.context.clone(),
+            stream,
+            &svr_cfg,
+            WrapAddress(addr),
+        )
+        .compat();
+        Ok(Box::new(WrapSSTcp(client)))
+    }
+
+    async fn tcp_bind(
+        &self,
+        _ctx: &mut rd_interface::Context,
+        _addr: Address,
+    ) -> Result<TcpListener> {
         Err(NOT_IMPLEMENTED)
     }
 
-    async fn tcp_bind(&self, _ctx: &mut Context, _addr: Address) -> Result<TcpListener> {
-        Err(NOT_IMPLEMENTED)
-    }
-
-    async fn udp_bind(&self, _ctx: &mut Context, _addr: Address) -> Result<UdpSocket> {
+    async fn udp_bind(
+        &self,
+        _ctx: &mut rd_interface::Context,
+        _addr: Address,
+    ) -> Result<UdpSocket> {
         Err(NOT_IMPLEMENTED)
     }
 }
 
 #[no_mangle]
 pub fn init_plugin(registry: &mut Registry) -> Result<()> {
-    registry.add_net("shadowsocks", |_, _| Ok(Net::new()));
+    registry.add_net("shadowsocks", |net, config| SSNet::new(net, config));
 
     Ok(())
 }
---
+++ ss/src/wrapper.rs
@@ -0,0 +1,122 @@
+use pin::Pin;
+use rd_interface::{
+    async_trait, Address as RDAddress, AsyncRead, AsyncWrite, ITcpStream, TcpStream,
+    NOT_IMPLEMENTED,
+};
+use serde::{
+    de::{self, Visitor},
+    Deserializer,
+};
+use shadowsocks::{crypto::v1::CipherKind, relay::socks5::Address as SSAddress, ProxyClientStream};
+use std::{io, net::SocketAddr, pin, task};
+use task::{Context, Poll};
+use tokio_util::compat::Compat;
+
+pub struct WrapAddress(pub RDAddress);
+
+impl From<RDAddress> for WrapAddress {
+    fn from(a: RDAddress) -> Self {
+        Self(a)
+    }
+}
+
+impl Into<SSAddress> for WrapAddress {
+    fn into(self) -> SSAddress {
+        match self.0 {
+            RDAddress::Domain(domain, port) => SSAddress::DomainNameAddress(domain, port),
+            RDAddress::IPv4(v4) => SSAddress::SocketAddress(SocketAddr::V4(v4)),
+            RDAddress::IPv6(v6) => SSAddress::SocketAddress(SocketAddr::V6(v6)),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct WrapCipher(CipherKind);
+
+impl Into<CipherKind> for WrapCipher {
+    fn into(self) -> CipherKind {
+        self.0
+    }
+}
+
+pub fn deserialize_cipher<'de, D>(de: D) -> Result<WrapCipher, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct StrVisitor<'a>(&'a std::marker::PhantomData<()>);
+    impl<'a, 'de> Visitor<'de> for StrVisitor<'a> {
+        type Value = WrapCipher;
+
+        fn expecting(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(
+                f,
+                "a chiper from aes-128-gcm aes-256-gcm
+aes-128-ctr aes-192-ctr aes-256-ctr
+rc4-md5 chacha20-ietf chacha20-ietf-poly1305"
+            )
+        }
+
+        fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            Ok(WrapCipher(match s {
+                "aes-128-gcm" => CipherKind::AES_128_GCM,
+                "aes-256-gcm" => CipherKind::AES_256_GCM,
+                "aes-128-ctr" => CipherKind::AES_128_CTR,
+                "aes-192-ctr" => CipherKind::AES_192_CTR,
+                "aes-256-ctr" => CipherKind::AES_256_CTR,
+                "rc4-md5" => CipherKind::SS_RC4_MD5,
+                "chacha20-ietf" => CipherKind::CHACHA20,
+                "chacha20-ietf-poly1305" => CipherKind::CHACHA20_POLY1305,
+
+                _ => return Err(de::Error::invalid_value(de::Unexpected::Str(s), &self)),
+            }))
+        }
+    }
+
+    de.deserialize_str(StrVisitor(&std::marker::PhantomData))
+}
+
+pub struct WrapSSTcp(pub Compat<ProxyClientStream<Compat<TcpStream>>>);
+
+impl AsyncRead for WrapSSTcp {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
+        Pin::new(&mut self.0).poll_read(cx, buf)
+    }
+}
+
+impl AsyncWrite for WrapSSTcp {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        Pin::new(&mut self.0).poll_write(cx, buf)
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Pin::new(&mut self.0).poll_flush(cx)
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Pin::new(&mut self.0).poll_close(cx)
+    }
+}
+
+#[async_trait]
+impl ITcpStream for WrapSSTcp {
+    async fn peer_addr(&self) -> rd_interface::Result<SocketAddr> {
+        Err(NOT_IMPLEMENTED)
+    }
+
+    async fn local_addr(&self) -> rd_interface::Result<SocketAddr> {
+        Err(NOT_IMPLEMENTED)
+    }
+}
+
+unsafe impl Sync for WrapSSTcp {}
