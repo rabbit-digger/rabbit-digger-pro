ccb9dc380e1b9c32d0090a98613dfb91c647f71d
spacemeowx2 <spacemeowx2@gmail.com>
2021-04-17 18:49:54 +0800

    feat: ignore redir when not linux

--- rd-redir/src/lib.rs
+++ rd-redir/src/lib.rs
@@ -1,71 +1,79 @@
 mod util;
-use std::net::SocketAddr;
 
-use crate::util::OriginAddrExt;
-use async_std::{
-    net::{TcpListener, TcpStream},
-    task::spawn,
-};
-use rd_interface::{
-    async_trait, config::from_value, context::common_field::SourceAddress, util::connect_tcp,
-    Context, IServer, IntoAddress, Net, Registry, Result,
-};
-use serde_derive::Deserialize;
+#[cfg(target_os = "linux")]
+use linux::{RedirServer, RedirServerConfig};
+use rd_interface::{config::from_value, Registry, Result};
 
-#[derive(Debug, Deserialize)]
-struct RedirServerConfig {
-    bind: String,
-}
+#[cfg(target_os = "linux")]
+mod linux {
+    use std::net::SocketAddr;
 
-struct RedirServer {
-    cfg: RedirServerConfig,
-    net: Net,
-}
+    use crate::util::OriginAddrExt;
+    use async_std::{
+        net::{TcpListener, TcpStream},
+        task::spawn,
+    };
+    use rd_interface::{
+        async_trait, context::common_field::SourceAddress, util::connect_tcp, Context, IServer,
+        IntoAddress, Net, Result,
+    };
+    use serde_derive::Deserialize;
 
-#[async_trait]
-impl IServer for RedirServer {
-    async fn start(&self) -> Result<()> {
-        let listener = TcpListener::bind(&self.cfg.bind).await?;
-        self.serve_listener(listener).await
+    #[derive(Debug, Deserialize)]
+    pub struct RedirServerConfig {
+        bind: String,
     }
 
-    async fn stop(&self) -> Result<()> {
-        // TODO
-        Ok(())
-    }
-}
-
-async fn new_context(addr: SocketAddr) -> Context {
-    let mut ctx = Context::new();
-    let _ = ctx
-        .insert_common::<SourceAddress>(SourceAddress { addr })
-        .await
-        .ok();
-    ctx
-}
-
-impl RedirServer {
-    fn new(cfg: RedirServerConfig, net: Net) -> Self {
-        RedirServer { cfg, net }
+    pub struct RedirServer {
+        cfg: RedirServerConfig,
+        net: Net,
     }
 
-    pub async fn serve_listener(&self, listener: TcpListener) -> Result<()> {
-        loop {
-            let (socket, addr) = listener.accept().await?;
-            let _ = spawn(Self::serve_connection(self.net.clone(), socket, addr));
+    #[async_trait]
+    impl IServer for RedirServer {
+        async fn start(&self) -> Result<()> {
+            let listener = TcpListener::bind(&self.cfg.bind).await?;
+            self.serve_listener(listener).await
+        }
+
+        async fn stop(&self) -> Result<()> {
+            // TODO
+            Ok(())
         }
     }
 
-    async fn serve_connection(net: Net, socket: TcpStream, addr: SocketAddr) -> Result<()> {
-        let target = socket.origin_addr()?;
+    async fn new_context(addr: SocketAddr) -> Context {
+        let mut ctx = Context::new();
+        let _ = ctx
+            .insert_common::<SourceAddress>(SourceAddress { addr })
+            .await
+            .ok();
+        ctx
+    }
 
-        let target_tcp = net
-            .tcp_connect(&mut new_context(addr).await, target.into_address()?)
-            .await?;
+    impl RedirServer {
+        pub fn new(cfg: RedirServerConfig, net: Net) -> Self {
+            RedirServer { cfg, net }
+        }
 
-        connect_tcp(socket, target_tcp).await?;
+        pub async fn serve_listener(&self, listener: TcpListener) -> Result<()> {
+            loop {
+                let (socket, addr) = listener.accept().await?;
+                let _ = spawn(Self::serve_connection(self.net.clone(), socket, addr));
+            }
+        }
 
-        Ok(())
+        async fn serve_connection(net: Net, socket: TcpStream, addr: SocketAddr) -> Result<()> {
+            let target = socket.origin_addr()?;
+
+            let target_tcp = net
+                .tcp_connect(&mut new_context(addr).await, target.into_address()?)
+                .await?;
+
+            connect_tcp(socket, target_tcp).await?;
+
+            Ok(())
+        }
     }
 }
 
