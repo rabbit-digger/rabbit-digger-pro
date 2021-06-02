329f66fef34e0a20a009c64e3692a61d25e17a96
spacemeowx2 <spacemeowx2@gmail.com>
2021-06-02 20:24:29 +0800

    test: add address test, fix address parsing

--- rd-interface/src/address.rs
+++ rd-interface/src/address.rs
@@ -28,6 +28,12 @@ fn no_addr() -> Error {
 }
 
 fn host_to_address(host: &str, port: u16) -> Address {
+    let host = host
+        .strip_prefix('[')
+        .map(|h| h.strip_suffix(']'))
+        .flatten()
+        .unwrap_or(host);
+
     match str::parse::<IpAddr>(host) {
         Ok(ip) => {
             let addr = SocketAddr::new(ip, port);
@@ -39,13 +45,13 @@ fn host_to_address(host: &str, port: u16) -> Address {
 
 impl IntoAddress for &str {
     fn into_address(self) -> Result<Address> {
-        let mut parts = self.splitn(2, ":");
-        let host = parts.next().ok_or_else(no_addr)?;
+        let mut parts = self.rsplitn(2, ":");
         let port: u16 = parts
             .next()
             .ok_or_else(no_addr)?
             .parse()
             .map_err(|_| no_addr())?;
+        let host = parts.next().ok_or_else(no_addr)?;
         Ok(host_to_address(host, port))
     }
 }
@@ -124,5 +130,55 @@ impl fmt::Display for Address {
             Address::Domain(domain, port) => write!(f, "{}:{}", domain, port),
             Address::SocketAddr(s) => write!(f, "{}", s),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::{Ipv4Addr, Ipv6Addr};
+
+    const IPV4_ADDR: IpAddr = IpAddr::V4(Ipv4Addr::new(1, 2, 3, 4));
+    const IPV6_ADDR: IpAddr = IpAddr::V6(Ipv6Addr::new(1, 2, 3, 4, 5, 6, 7, 8));
+    const DOMAIN: &'static str = "example.com";
+
+    #[test]
+    fn test_address_convert() {
+        let ipv4_addr = Address::SocketAddr(SocketAddr::new(IPV4_ADDR, 1234));
+        let ipv6_addr = Address::SocketAddr(SocketAddr::new(IPV6_ADDR, 1234));
+        let domain_addr = Address::Domain(DOMAIN.to_string(), 1234);
+
+        // &str
+        assert_eq!(ipv4_addr, "1.2.3.4:1234".into_address().unwrap());
+        assert_eq!(ipv6_addr, "[1:2:3:4:5:6:7:8]:1234".into_address().unwrap());
+        assert_eq!(domain_addr, "example.com:1234".into_address().unwrap());
+
+        // (&str, u16)
+        assert_eq!(ipv4_addr, ("1.2.3.4", 1234).into_address().unwrap());
+        assert_eq!(
+            ipv6_addr,
+            ("[1:2:3:4:5:6:7:8]", 1234).into_address().unwrap()
+        );
+        assert_eq!(domain_addr, ("example.com", 1234).into_address().unwrap());
+
+        // (String, u16)
+        assert_eq!(
+            ipv4_addr,
+            ("1.2.3.4".to_string(), 1234).into_address().unwrap()
+        );
+        assert_eq!(
+            ipv6_addr,
+            ("[1:2:3:4:5:6:7:8]".to_string(), 1234)
+                .into_address()
+                .unwrap()
+        );
+        assert_eq!(
+            domain_addr,
+            ("example.com".to_string(), 1234).into_address().unwrap()
+        );
+
+        // (IpAddr, u16)
+        assert_eq!(ipv4_addr, (IPV4_ADDR, 1234).into_address().unwrap());
+        assert_eq!(ipv6_addr, (IPV6_ADDR, 1234).into_address().unwrap());
     }
 }
