bcb2f4e3359afe9ab39fabcea8239d0e9ad919c7
spacemeowx2 <spacemeowx2@gmail.com>
2021-06-08 22:04:29 +0800

    feat: add impl_async_write

--- rd-interface/src/macros.rs
+++ rd-interface/src/macros.rs
@@ -1,6 +1,14 @@
 #[macro_export]
 macro_rules! impl_async_read_write {
     ($s:ident, $f:tt) => {
+        rd_interface::impl_async_read!($s, $f);
+        rd_interface::impl_async_write!($s, $f);
+    };
+}
+
+#[macro_export]
+macro_rules! impl_async_read {
+    ($s:ident, $f:tt) => {
         impl ::rd_interface::AsyncRead for $s {
             fn poll_read(
                 mut self: ::std::pin::Pin<&mut Self>,
@@ -10,6 +18,12 @@ macro_rules! impl_async_read_write {
                 ::rd_interface::AsyncRead::poll_read(::std::pin::Pin::new(&mut self.$f), cx, buf)
             }
         }
+    };
+}
+
+#[macro_export]
+macro_rules! impl_async_write {
+    ($s:ident, $f:tt) => {
         impl ::rd_interface::AsyncWrite for $s {
             fn poll_write(
                 mut self: ::std::pin::Pin<&mut Self>,
@@ -31,21 +45,6 @@ macro_rules! impl_async_read_write {
                 cx: &mut ::std::task::Context<'_>,
             ) -> ::std::task::Poll<::std::io::Result<()>> {
                 ::rd_interface::AsyncWrite::poll_shutdown(::std::pin::Pin::new(&mut self.$f), cx)
-            }
-        }
-    };
-}
-
-#[macro_export]
-macro_rules! impl_async_read {
-    ($s:ident, $f:tt) => {
-        impl ::rd_interface::AsyncRead for $s {
-            fn poll_read(
-                mut self: ::std::pin::Pin<&mut Self>,
-                cx: &mut ::std::task::Context<'_>,
-                buf: &mut ::rd_interface::ReadBuf,
-            ) -> ::std::task::Poll<::std::io::Result<()>> {
-                ::rd_interface::AsyncRead::poll_read(::std::pin::Pin::new(&mut self.$f), cx, buf)
             }
         }
     };
