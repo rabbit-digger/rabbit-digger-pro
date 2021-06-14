8f7eccd972a50dda28a9feaa15d8853040f99555
spacemeowx2 <spacemeowx2@gmail.com>
2021-06-15 07:40:32 +0800

    feat: be able to accept tcp

--- protocol/raw/src/gateway.rs
+++ protocol/raw/src/gateway.rs
@@ -1,29 +1,178 @@
 use std::{
-    net::SocketAddr,
+    net::SocketAddrV4,
     pin::Pin,
     sync::Arc,
     task::{Context, Poll},
 };
 
 use dashmap::DashMap;
-use futures::{Sink, SinkExt, Stream, StreamExt};
+use futures::{ready, Sink, SinkExt, Stream, StreamExt};
 use rd_interface::Result;
+use smoltcp::wire::{
+    EthernetFrame, EthernetProtocol, IpProtocol, Ipv4Packet, TcpPacket, UdpPacket,
+};
 use tokio_smoltcp::device::{Interface, Packet};
 
-pub struct GatewayInterface<I> {
-    inner: I,
-    map: Arc<DashMap<SocketAddr, SocketAddr>>,
+#[derive(Clone)]
+pub struct MapTable {
+    map: Arc<DashMap<SocketAddrV4, SocketAddrV4>>,
 }
 
-impl<I> GatewayInterface<I> {
-    pub fn new(inner: I) -> GatewayInterface<I> {
-        GatewayInterface {
-            inner,
+impl MapTable {
+    fn new() -> MapTable {
+        MapTable {
             map: Arc::new(DashMap::new()),
         }
     }
-    pub fn get_map(&self) -> Arc<DashMap<SocketAddr, SocketAddr>> {
+    fn insert(&self, key: SocketAddrV4, value: SocketAddrV4) {
+        self.map.insert(key, value);
+    }
+    pub fn get(&self, key: &SocketAddrV4) -> Option<SocketAddrV4> {
+        self.map.get(key).map(|i| *i)
+    }
+}
+
+pub struct GatewayInterface<I> {
+    inner: I,
+    map: MapTable,
+
+    override_v4: SocketAddrV4,
+}
+
+fn set_dst_addr<T: AsRef<[u8]> + AsMut<[u8]>>(
+    ip: &mut Ipv4Packet<T>,
+    dst_addr: SocketAddrV4,
+) -> smoltcp::Result<Option<(SocketAddrV4, SocketAddrV4)>> {
+    let src_addr = ip.src_addr();
+    let orig_addr = ip.dst_addr();
+
+    let (src_port, orig_port) = match ip.protocol() {
+        IpProtocol::Tcp => {
+            ip.set_dst_addr(dst_addr.ip().to_owned().into());
+
+            let mut tcp = TcpPacket::new_unchecked(ip.payload_mut());
+            let dst_port = tcp.dst_port();
+            tcp.set_dst_port(dst_addr.port());
+
+            (tcp.src_port(), dst_port)
+        }
+        IpProtocol::Udp => {
+            ip.set_dst_addr(dst_addr.ip().to_owned().into());
+
+            let mut udp = UdpPacket::new_unchecked(ip.payload_mut());
+            let dst_port = udp.dst_port();
+            udp.set_dst_port(dst_addr.port());
+
+            (udp.src_port(), dst_port)
+        }
+        _ => return Ok(None),
+    };
+
+    Ok(Some((
+        SocketAddrV4::new(src_addr.into(), src_port),
+        SocketAddrV4::new(orig_addr.into(), orig_port),
+    )))
+}
+
+fn get_dst_addr<T: AsRef<[u8]> + AsMut<[u8]>>(
+    ip: &mut Ipv4Packet<T>,
+) -> smoltcp::Result<Option<SocketAddrV4>> {
+    let dst_addr = ip.dst_addr();
+    let port = match ip.protocol() {
+        IpProtocol::Tcp => TcpPacket::new_checked(ip.payload_mut())?.dst_port(),
+        IpProtocol::Udp => UdpPacket::new_checked(ip.payload_mut())?.dst_port(),
+        _ => return Ok(None),
+    };
+    Ok(Some(SocketAddrV4::new(dst_addr.into(), port)))
+}
+
+fn set_src_addr<T: AsRef<[u8]> + AsMut<[u8]>>(
+    ip: &mut Ipv4Packet<T>,
+    src_addr_v4: SocketAddrV4,
+) -> smoltcp::Result<()> {
+    let src_addr = src_addr_v4.ip().to_owned().into();
+    let dst_addr = ip.dst_addr();
+    let port = src_addr_v4.port();
+    match ip.protocol() {
+        IpProtocol::Tcp => {
+            ip.set_src_addr(src_addr);
+
+            let mut tcp = TcpPacket::new_checked(ip.payload_mut())?;
+            tcp.set_src_port(port);
+
+            tcp.fill_checksum(&src_addr.into(), &dst_addr.into());
+        }
+        IpProtocol::Udp => {
+            ip.set_src_addr(src_addr);
+
+            let mut udp = UdpPacket::new_checked(ip.payload_mut())?;
+            udp.set_src_port(port);
+
+            udp.fill_checksum(&src_addr.into(), &dst_addr.into());
+        }
+        _ => {}
+    };
+    ip.fill_checksum();
+
+    Ok(())
+}
+
+impl<I> GatewayInterface<I> {
+    pub fn new(inner: I, override_v4: SocketAddrV4) -> GatewayInterface<I> {
+        GatewayInterface {
+            inner,
+            map: MapTable::new(),
+
+            override_v4,
+        }
+    }
+    pub fn get_map(&self) -> MapTable {
         self.map.clone()
+    }
+    fn map_in(&self, mut packet: Packet) -> Packet {
+        fn map(
+            packet: &mut Packet,
+            override_v4: SocketAddrV4,
+            map_table: &MapTable,
+        ) -> smoltcp::Result<()> {
+            let mut frame = EthernetFrame::new_checked(packet)?;
+
+            match frame.ethertype() {
+                EthernetProtocol::Ipv4 => {
+                    let mut ipv4 = Ipv4Packet::new_checked(frame.payload_mut())?;
+                    if let Some((src_addr, ori_addr)) = set_dst_addr(&mut ipv4, override_v4)? {
+                        map_table.insert(src_addr, ori_addr);
+                    }
+                }
+                _ => return Ok(()),
+            };
+
+            Ok(())
+        }
+        map(&mut packet, self.override_v4, &self.map).ok();
+        packet
+    }
+    fn map_out(&self, mut packet: Packet) -> Packet {
+        fn map(packet: &mut Packet, map_table: &MapTable) -> smoltcp::Result<()> {
+            let mut frame = EthernetFrame::new_checked(packet)?;
+
+            match frame.ethertype() {
+                EthernetProtocol::Ipv4 => {
+                    let mut ipv4 = Ipv4Packet::new_checked(frame.payload_mut())?;
+                    if let Some(src) = get_dst_addr(&mut ipv4)?
+                        .map(|d| map_table.get(&d))
+                        .flatten()
+                    {
+                        set_src_addr(&mut ipv4, src)?;
+                    }
+                }
+                _ => return Ok(()),
+            };
+
+            Ok(())
+        }
+        map(&mut packet, &self.map).ok();
+        packet
     }
 }
 
@@ -34,7 +183,8 @@ where
     type Item = I::Item;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        self.inner.poll_next_unpin(cx)
+        let item = ready!(self.inner.poll_next_unpin(cx));
+        Poll::Ready(item.map(|p| self.map_in(p)))
     }
 }
 
@@ -49,6 +199,7 @@ where
     }
 
     fn start_send(mut self: Pin<&mut Self>, item: Packet) -> Result<(), Self::Error> {
+        let item = self.map_out(item);
         self.inner.start_send_unpin(item)
     }
 
--- protocol/raw/src/server.rs
+++ protocol/raw/src/server.rs
@@ -1,34 +1,63 @@
-use std::{net::SocketAddr, str::FromStr};
+use std::{
+    net::{SocketAddr, SocketAddrV4},
+    str::FromStr,
+};
 
-use futures::future::pending;
+use futures::{future::ready, StreamExt};
 use rd_interface::{
     async_trait,
     registry::ServerFactory,
     schemars::{self, JsonSchema},
-    Arc, Config, Error, IServer, Net, Result,
+    util::connect_tcp,
+    Config, Context, Error, IServer, IntoAddress, Net, Result,
 };
 use serde_derive::{Deserialize, Serialize};
-use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr};
-use tokio::select;
-use tokio_smoltcp::{device::FutureDevice, BufferSize, NetConfig, TcpListener, UdpSocket};
+use smoltcp::wire::{EthernetFrame, EthernetProtocol, Ipv4Packet};
+use smoltcp::{
+    phy::ChecksumCapabilities,
+    wire::{EthernetAddress, IpCidr},
+};
+use tokio::{select, spawn};
+use tokio_smoltcp::{
+    device::{FutureDevice, Packet},
+    BufferSize, NetConfig, TcpListener, UdpSocket,
+};
 
-use dashmap::DashMap;
-
-use crate::{device, gateway::GatewayInterface};
+use crate::{
+    device,
+    gateway::{GatewayInterface, MapTable},
+};
 
 #[derive(Serialize, Deserialize, JsonSchema, Config)]
 pub struct RawServerConfig {
     device: String,
     mtu: usize,
-    ethernet_addr: String,
     ip_addr: String,
-    gateway: String,
+    ethernet_addr: String,
 }
 
 pub struct RawServer {
     net: Net,
     smoltcp_net: tokio_smoltcp::Net,
-    map: Arc<DashMap<SocketAddr, SocketAddr>>,
+    map: MapTable,
+}
+
+fn filter_packet(packet: &Packet, ethernet_addr: EthernetAddress, ip_addr: IpCidr) -> bool {
+    if let Ok(f) = EthernetFrame::new_checked(packet) {
+        let ether_accept = f.dst_addr() == ethernet_addr || f.dst_addr().is_broadcast();
+        ether_accept && {
+            match (f.ethertype(), Ipv4Packet::new_checked(f.payload())) {
+                (EthernetProtocol::Ipv4, Ok(p)) => {
+                    ip_addr.contains_addr(&p.src_addr().into())
+                        || ip_addr.contains_addr(&p.dst_addr().into())
+                }
+                // ignore other ether type
+                _ => true,
+            }
+        }
+    } else {
+        false
+    }
 }
 
 impl RawServer {
@@ -37,8 +66,7 @@ impl RawServer {
             .map_err(|_| Error::Other("Failed to parse ethernet_addr".into()))?;
         let ip_addr = IpCidr::from_str(&config.ip_addr)
             .map_err(|_| Error::Other("Failed to parse ip_addr".into()))?;
-        let gateway = IpAddress::from_str(&config.gateway)
-            .map_err(|_| Error::Other("Failed to parse gateway".into()))?;
+        let gateway = ip_addr.address();
         let device = device::get_device(&config.device)?;
 
         let net_config = NetConfig {
@@ -51,10 +79,22 @@ impl RawServer {
                 ..Default::default()
             },
         };
-        let device = GatewayInterface::new(device::get_by_device(device)?);
+
+        let addr = match ip_addr {
+            IpCidr::Ipv4(v4) => v4.address(),
+            _ => return Err(Error::Other("Ipv6 is not supported".into())),
+        };
+
+        let device = GatewayInterface::new(
+            device::get_by_device(device)?
+                .filter(move |p: &Packet| ready(filter_packet(p, ethernet_addr, ip_addr))),
+            SocketAddrV4::new(addr.into(), 20000),
+        );
         let map = device.get_map();
         let mut device = FutureDevice::new(device, config.mtu);
         device.caps.max_burst_size = Some(100);
+        // ignored checksum since we modify the packets
+        device.caps.checksum = ChecksumCapabilities::ignored();
 
         let (smoltcp_net, fut) = tokio_smoltcp::Net::new(device, net_config);
         tokio::spawn(fut);
@@ -65,16 +105,54 @@ impl RawServer {
             map,
         })
     }
-    async fn serve_tcp(&self, _listener: TcpListener) -> Result<()> {
-        pending::<()>().await;
-        Ok(())
+    async fn serve_tcp(&self, mut listener: TcpListener) -> Result<()> {
+        loop {
+            let (tcp, addr) = listener.accept().await?;
+            let orig_addr = self.map.get(&match addr {
+                SocketAddr::V4(v4) => v4,
+                _ => continue,
+            });
+            if let Some(orig_addr) = orig_addr {
+                let net = self.net.clone();
+                spawn(async move {
+                    let target = net
+                        .tcp_connect(
+                            &mut Context::new(),
+                            SocketAddr::from(orig_addr).into_address()?,
+                        )
+                        .await?;
+                    connect_tcp(tcp, target).await?;
+                    Ok(()) as Result<()>
+                });
+            }
+        }
     }
     async fn serve_udp(&self, udp: UdpSocket) -> Result<()> {
         let mut buf = [0u8; 2048];
+        let nudp = self
+            .net
+            .udp_bind(&mut Context::new(), "0.0.0.0:0".into_address()?)
+            .await?;
         loop {
             let (size, addr) = udp.recv_from(&mut buf).await?;
-            // let buf = &buf[..size];
-            tracing::trace!("recv from {} size {}", addr, size);
+            let orig_addr = self.map.get(&match addr {
+                SocketAddr::V4(v4) => v4,
+                _ => continue,
+            });
+            if let Some(orig_addr) = orig_addr {
+                tracing::trace!(
+                    "recv from {} size {}, orig_addr {:?}",
+                    addr,
+                    size,
+                    orig_addr
+                );
+                nudp.send_to(&buf[..size], SocketAddr::from(orig_addr).into_address()?)
+                    .await?;
+
+                let (size, recv_addr) = nudp.recv_from(&mut buf).await?;
+                tracing::trace!("recv_from other side {}", recv_addr);
+                udp.send_to(&buf[..size], addr).await?;
+            }
         }
     }
 }
@@ -84,7 +162,7 @@ impl IServer for RawServer {
     async fn start(&self) -> Result<()> {
         let tcp_listener = self
             .smoltcp_net
-            .tcp_bind("0.0.0.0:10000".parse().unwrap())
+            .tcp_bind("0.0.0.0:20000".parse().unwrap())
             .await?;
         let udp_listener = self
             .smoltcp_net
