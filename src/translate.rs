6cef3bb7124968ab9aff25f21db8a5446a927e45
spacemeowx2 <spacemeowx2@gmail.com>
2021-06-27 02:31:38 +0800

    build: remove translate feature

--- src/translate.rs
+++ src/translate.rs
@@ -5,12 +5,6 @@ use anyhow::{anyhow, Result};
 use rabbit_digger::config::Config;
 use tokio::fs::read_to_string;
 
-#[cfg(not(feature = "translate"))]
-pub async fn post_process(_config: &mut Config, _import: Import) -> Result<()> {
-    Ok(())
-}
-
-#[cfg(feature = "translate")]
 pub async fn post_process(config: &mut Config, import: Import) -> Result<()> {
     let content = read_to_string(import.path).await?;
     match import.format.as_ref() {
