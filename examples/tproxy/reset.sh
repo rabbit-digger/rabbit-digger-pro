fb592484d6056e30e9eefeecc93df5ad64898a36
spacemeowx2 <spacemeowx2@gmail.com>
2021-09-02 23:32:00 +0800

    feat: update script

--- examples/tproxy/reset.sh
+++ examples/tproxy/reset.sh
@@ -1,23 +1,22 @@
-#!/bin/bash
-set -e
+#!/bin/sh
+# set -x
 
-RD_FW_MARK=0x1919
-RD_TABLE=100
+RD_FW_MARK="${RD_FW_MARK:=0xfe}"
+RD_TABLE="${RD_TABLE:=101}"
 
-if [[ $EUID -ne 0 ]]; then
+if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-iptables -t mangle -D OUTPUT -j RD-MARK
+iptables -t mangle -D OUTPUT -j RD_OUTPUT
+iptables -t mangle -D PREROUTING -j RD_PREROUTING
 
-iptables -t mangle -F RD-MARK
-iptables -t mangle -X RD-MARK
+iptables -t mangle -F RD_PREROUTING
+iptables -t mangle -X RD_PREROUTING
 
-iptables -t mangle -D PREROUTING -j RD
-
-iptables -t mangle -F RD
-iptables -t mangle -X RD
+iptables -t mangle -F RD_OUTPUT
+iptables -t mangle -X RD_OUTPUT
 
 ip -4 rule del fwmark $RD_FW_MARK table $RD_TABLE
 ip -4 route del local 0/0 dev lo table $RD_TABLE
