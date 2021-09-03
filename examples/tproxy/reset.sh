772adc2475a5a68d76c3af6e5ac56587227e1866
spacemeowx2 <spacemeowx2@gmail.com>
2021-09-04 06:03:50 +0800

    feat: add ipv6 support

--- examples/tproxy/reset.sh
+++ examples/tproxy/reset.sh
@@ -3,10 +3,25 @@
 
 RD_FW_MARK="${RD_FW_MARK:=0xfe}"
 RD_TABLE="${RD_TABLE:=101}"
+RD_DISABLE_IPV6="${RD_DISABLE_IPV6:=0}"
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
+fi
+
+if [ "$RD_DISABLE_IPV6" != "1" ]; then
+   ip6tables -t mangle -D OUTPUT -j RD_OUTPUT
+   ip6tables -t mangle -D PREROUTING -j RD_PREROUTING
+
+   ip6tables -t mangle -F RD_PREROUTING
+   ip6tables -t mangle -X RD_PREROUTING
+
+   ip6tables -t mangle -F RD_OUTPUT
+   ip6tables -t mangle -X RD_OUTPUT
+
+   ip -6 rule del fwmark $RD_FW_MARK table $RD_TABLE
+   ip -6 route del local ::/0 dev lo table $RD_TABLE
 fi
 
 iptables -t mangle -D OUTPUT -j RD_OUTPUT
--- examples/tproxy/setup.sh
+++ examples/tproxy/setup.sh
@@ -3,10 +3,12 @@
 
 RD_MARK="${RD_MARK:=0x11}"
 RD_PORT="${RD_PORT:=19810}"
+RD_PORT6="${RD_PORT6:=19811}"
 RD_TABLE="${RD_TABLE:=101}"
 RD_FW_MARK="${RD_FW_MARK:=0xfe}"
 RD_CT_MARK="${RD_CT_MARK:=0x10}"
 RD_INTERNAL_DEV="${RD_INTERNAL_DEV:=br-lan}"
+RD_DISABLE_IPV6="${RD_DISABLE_IPV6:=0}"
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
@@ -17,28 +19,62 @@ fi
 ip -4 route add local 0/0 dev lo table $RD_TABLE
 ip -4 rule add fwmark $RD_FW_MARK table $RD_TABLE
 
+if [ "$RD_DISABLE_IPV6" != "1" ]; then
+   ip -6 route add local ::/0 dev lo table $RD_TABLE
+   ip -6 rule add fwmark $RD_FW_MARK table $RD_TABLE
+   
+   ip6tables -t mangle -N RD_OUTPUT
+   # if RD_INTERNAL_DEV is existed
+   if [ -d /sys/class/net/$RD_INTERNAL_DEV ]; then
+      ip6tables -t mangle -A RD_OUTPUT ! -i $RD_INTERNAL_DEV -j RETURN
+   else
+      echo "Warning: Internet interface $RD_INTERNAL_DEV is not found, the traffic may mess up."
+   fi
+   ip6tables -t mangle -A RD_OUTPUT -d ::1/128 -j RETURN
+   ip6tables -t mangle -A RD_OUTPUT -d fc00::/7 -j RETURN
+   ip6tables -t mangle -A RD_OUTPUT -d fe80::/10 -j RETURN
+   ip6tables -t mangle -A RD_OUTPUT -j RETURN -m mark --mark $RD_MARK
+   ip6tables -t mangle -A RD_OUTPUT -p udp -j MARK --set-mark $RD_FW_MARK
+   ip6tables -t mangle -A RD_OUTPUT -p tcp -j MARK --set-mark $RD_FW_MARK
+
+   ip6tables -t mangle -N RD_PREROUTING
+   # if RD_INTERNAL_DEV is existed
+   if [ -d /sys/class/net/$RD_INTERNAL_DEV ]; then
+      ip6tables -t mangle -A RD_PREROUTING ! -i $RD_INTERNAL_DEV -j RETURN
+   fi
+   ip6tables -t mangle -A RD_PREROUTING -d ::1/128 -j RETURN
+   ip6tables -t mangle -A RD_PREROUTING -d fc00::/7 -j RETURN
+   ip6tables -t mangle -A RD_PREROUTING -d fe80::/10 -j RETURN
+   ip6tables -t mangle -A RD_PREROUTING -m mark --mark $RD_MARK -j RETURN
+   ip6tables -t mangle -A RD_PREROUTING -j TPROXY -p udp --on-port $RD_PORT6 --tproxy-mark $RD_FW_MARK
+   ip6tables -t mangle -A RD_PREROUTING -j TPROXY -p tcp --on-port $RD_PORT6 --tproxy-mark $RD_FW_MARK
+
+   ip6tables -t mangle -A OUTPUT -j RD_OUTPUT
+   ip6tables -t mangle -A PREROUTING -j RD_PREROUTING
+fi
+
 iptables -t mangle -N RD_OUTPUT
 # if RD_INTERNAL_DEV is existed
-if [ -n /sys/class/net/$RD_INTERNAL_DEV ]; then
+if [ -d /sys/class/net/$RD_INTERNAL_DEV ]; then
    iptables -t mangle -A RD_OUTPUT ! -i $RD_INTERNAL_DEV -j RETURN
 else
-   echo "Warning: Internet interface `$RD_INTERNAL_DEV` is not found, the traffic may mess up."
+   echo "Warning: Internet interface $RD_INTERNAL_DEV is not found, the traffic may mess up."
 fi
-iptables -t mangle -A RD_OUTPUT -s 0/8 -j RETURN
-iptables -t mangle -A RD_OUTPUT -s 127/8 -j RETURN
-iptables -t mangle -A RD_OUTPUT -s 10/8 -j RETURN
-iptables -t mangle -A RD_OUTPUT -s 169.254/16 -j RETURN
-iptables -t mangle -A RD_OUTPUT -s 172.16/12 -j RETURN
-iptables -t mangle -A RD_OUTPUT -s 192.168/16 -j RETURN
-iptables -t mangle -A RD_OUTPUT -s 224/4 -j RETURN
-iptables -t mangle -A RD_OUTPUT -s 240/4 -j RETURN
+iptables -t mangle -A RD_OUTPUT -d 0/8 -j RETURN
+iptables -t mangle -A RD_OUTPUT -d 127/8 -j RETURN
+iptables -t mangle -A RD_OUTPUT -d 10/8 -j RETURN
+iptables -t mangle -A RD_OUTPUT -d 169.254/16 -j RETURN
+iptables -t mangle -A RD_OUTPUT -d 172.16/12 -j RETURN
+iptables -t mangle -A RD_OUTPUT -d 192.168/16 -j RETURN
+iptables -t mangle -A RD_OUTPUT -d 224/4 -j RETURN
+iptables -t mangle -A RD_OUTPUT -d 240/4 -j RETURN
 iptables -t mangle -A RD_OUTPUT -j RETURN -m mark --mark $RD_MARK
 iptables -t mangle -A RD_OUTPUT -p udp -j MARK --set-mark $RD_FW_MARK
 iptables -t mangle -A RD_OUTPUT -p tcp -j MARK --set-mark $RD_FW_MARK
 
 iptables -t mangle -N RD_PREROUTING
 # if RD_INTERNAL_DEV is existed
-if [ -e /sys/class/net/$RD_INTERNAL_DEV ]; then
+if [ -d /sys/class/net/$RD_INTERNAL_DEV ]; then
    iptables -t mangle -A RD_PREROUTING ! -i $RD_INTERNAL_DEV -j RETURN
 fi
 iptables -t mangle -A RD_PREROUTING -d 0/8 -j RETURN
