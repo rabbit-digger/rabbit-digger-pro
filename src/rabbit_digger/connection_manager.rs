3e57222ea4eeafe2d65cb45cec1fd85f098c9fab
spacemeowx2 <spacemeowx2@gmail.com>
2021-09-14 22:26:55 -0700

    feat: handle events

--- src/rabbit_digger/connection_manager.rs
+++ src/rabbit_digger/connection_manager.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, sync::Arc};
+#![allow(dead_code)]
+use std::{collections::HashMap, sync::Arc, time::SystemTime};
 
 use super::event::{Event, EventType};
 use parking_lot::Mutex;
@@ -6,11 +7,26 @@ use rd_interface::{Address, Value};
 use serde::Serialize;
 use uuid::Uuid;
 
+fn ts(time: &SystemTime) -> u64 {
+    time.duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub enum Protocol {
+    Tcp,
+    Udp,
+}
+
 #[derive(Debug, Clone, Serialize)]
 pub struct ConnectionInfo {
-    uuid: Uuid,
+    protocol: Protocol,
     addr: Address,
+    start_time: u64,
     ctx: Value,
+    upload: usize,
+    download: usize,
 }
 
 struct Inner {
@@ -25,7 +41,9 @@ impl Inner {
     }
     fn _input_event(&self, conn: &mut HashMap<Uuid, ConnectionInfo>, event: &Event) {
         let Event {
-            uuid, event_type, ..
+            uuid,
+            event_type,
+            time,
         } = event;
 
         let uuid = *uuid;
@@ -34,9 +52,12 @@ impl Inner {
                 conn.insert(
                     uuid,
                     ConnectionInfo {
-                        uuid,
+                        protocol: Protocol::Tcp,
                         addr: addr.clone(),
                         ctx: ctx.clone(),
+                        start_time: ts(time),
+                        upload: 0,
+                        download: 0,
                     },
                 );
             }
@@ -44,16 +65,38 @@ impl Inner {
                 conn.insert(
                     uuid,
                     ConnectionInfo {
-                        uuid,
+                        protocol: Protocol::Udp,
                         addr: addr.clone(),
                         ctx: ctx.clone(),
+                        start_time: ts(time),
+                        upload: 0,
+                        download: 0,
                     },
                 );
+            }
+            EventType::Inbound(download) => {
+                if let Some(conn) = conn.get_mut(&uuid) {
+                    conn.download += download;
+                }
+            }
+            EventType::Outbound(upload) => {
+                if let Some(conn) = conn.get_mut(&uuid) {
+                    conn.upload += upload;
+                }
+            }
+            EventType::UdpInbound(_, download) => {
+                if let Some(conn) = conn.get_mut(&uuid) {
+                    conn.download += download;
+                }
+            }
+            EventType::UdpOutbound(_, upload) => {
+                if let Some(conn) = conn.get_mut(&uuid) {
+                    conn.upload += upload;
+                }
             }
             EventType::CloseConnection => {
                 conn.remove(&uuid);
             }
-            _ => {}
         };
     }
     fn input_event(&self, event: &Event) {
