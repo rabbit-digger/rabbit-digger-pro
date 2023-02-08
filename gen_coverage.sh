2c3f1ecf3e7581aef297d3cbfbdf2605123e1863
spacemeowx2 <spacemeowx2@gmail.com>
2023-02-08 20:19:42 +0800

    refactor: moving the package location

--- gen_coverage.sh
+++ gen_coverage.sh
@@ -1,1 +1,3 @@
+# Make sure you have cargo-tarpaulin installed: `cargo install cargo-tarpaulin`
+
 cargo tarpaulin --target-dir ./coverage/target --workspace --out Lcov --output-dir coverage --skip-clean
--- rabbit-digger/gen_coverage.sh
+++
@@ -1,1 +0,0 @@
-cargo tarpaulin --target-dir ./coverage/target --all-features --workspace -e rd-derive --out Lcov --output-dir coverage
