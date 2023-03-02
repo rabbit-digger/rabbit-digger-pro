43cd299f71adc261e7d57a31af52b096bd9f7d77
spacemeowx2 <spacemeowx2@gmail.com>
2023-03-02 21:37:01 +0800

    style: fix lint

--- rd-std/src/builtin/local.rs
+++ rd-std/src/builtin/local.rs
@@ -1,7 +1,6 @@
 use std::{
     io,
     net::{IpAddr, SocketAddr},
-    num::NonZeroU32,
     pin::Pin,
     task::{self, Poll},
     time::Duration,
@@ -143,7 +142,7 @@ impl LocalNetConfig {
                     return Err(io::Error::last_os_error().into());
                 }
 
-                socket.bind_device_by_index(NonZeroU32::new(idx))?;
+                socket.bind_device_by_index(std::num::NonZeroU32::new(idx))?;
             }
         }
 
