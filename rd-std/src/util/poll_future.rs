b4f4986a183f0d14f5b81acb89bbef9b2c0b9cc9
spacemeowx2 <spacemeowx2@gmail.com>
2023-08-31 02:16:14 +0800

    refactor(PollFuture): remove T: Clone

--- rd-std/src/util/poll_future.rs
+++ rd-std/src/util/poll_future.rs
@@ -1,18 +1,17 @@
 use std::{
     future::Future,
+    mem::replace,
     pin::Pin,
     task::{Context, Poll},
 };
 
 pub enum PollFuture<T> {
+    Empty,
     Ready(T),
     Future(Pin<Box<dyn Future<Output = T> + Send>>),
 }
 
-impl<T> PollFuture<T>
-where
-    T: Clone,
-{
+impl<T> PollFuture<T> {
     pub fn new(fut: impl Future<Output = T> + Send + 'static) -> Self {
         PollFuture::Future(Box::pin(fut))
     }
@@ -21,16 +20,24 @@ where
     }
     pub fn poll(&mut self, cx: &mut Context<'_>) -> Poll<T> {
         match self {
-            PollFuture::Ready(val) => Poll::Ready(val.clone()),
+            PollFuture::Empty => panic!("PollFuture::poll called on empty future"),
+            PollFuture::Ready(_) => {
+                let val = replace(self, PollFuture::Empty);
+                Poll::Ready(match val {
+                    PollFuture::Ready(val) => val,
+                    _ => unreachable!(),
+                })
+            }
             PollFuture::Future(fut) => {
                 let val = futures::ready!(fut.as_mut().poll(cx));
-                *self = PollFuture::Ready(val.clone());
+                *self = PollFuture::Empty;
                 Poll::Ready(val)
             }
         }
     }
     pub fn is_ready(&self) -> bool {
         match self {
+            PollFuture::Empty => true,
             PollFuture::Ready(_) => true,
             PollFuture::Future(_) => false,
         }
